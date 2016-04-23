45cc7304ce4dadc6efa86586c7931e4b879c6f38
postscript07@gmail.com <postscript07@gmail.com>
2016-04-23 10:04:17 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -57,15 +57,7 @@ module ActivateApp
     get '/gh' do
       redirect 'https://www.google.co.uk/maps/place/Greenhouse/@51.5529027,-0.0879017,15z/data=!4m2!3m1!1s0x0:0x9850520d11f22809'
     end
-    
-    get '/lumen' do
-      erb :lumen
-    end
-    
-    get '/about' do
-      erb :about
-    end
-    
+            
     get '/:slug' do
       if @fragment = Fragment.find_by(slug: params[:slug], page: true)
         erb :page
