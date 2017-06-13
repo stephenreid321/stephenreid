2ac163f155036f7e9ca2bf9bec8e2e8a63386881
Stephen Reid <postscript07@gmail.com>
2017-06-13 09:33:43 +0000

    .

--- models/holding.rb
+++ models/holding.rb
@@ -5,16 +5,25 @@ class Holding
   field :description, :type => String
   field :currency, :type => String
   field :amount, :type => Float
+    
+  def btc_per_unit    
+    return 1 if currency == 'bitcoin'
+    Mechanize.new.get("https://www.coinmath.com/#{currency}/btc").search('.price.number-with-commas')[0].text.to_f
+  end
+  
+  def self.gbp_per_btc
+    Mechanize.new.get("http://bittybot.co/uk/sell-bitcoins-uk/").search('#bb-tablepress-sell tr')[10].search('td')[4].text.gsub('£','').gsub(',','').to_f
+  end  
   
   def gbp_per_unit
-    Mechanize.new.get("https://www.coinmath.com/#{currency}/gbp").search('.price.number-with-commas')[0].text.to_f
+    btc_per_unit * Holding.gbp_per_btc
   end
   
   def valuation
     (amount*gbp_per_unit).round
   end
   
-  validates_presence_of :currency, :amount
+  validates_presence_of :description, :currency, :amount
         
   def self.admin_fields
     {
