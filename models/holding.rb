29285623d0277d4316e83a9d1bff855700babb99
postscript07@gmail.com <postscript07@gmail.com>
2017-11-30 14:47:24 -0600

    .

--- models/holding.rb
+++ models/holding.rb
@@ -11,16 +11,16 @@ class Holding
     Mechanize.new.get("https://www.coinmath.com/#{currency}/btc").search('.price.number-with-commas')[0].text.to_f
   end
   
-  def self.gbp_per_btc
-    Mechanize.new.get("http://bittybot.co/uk/sell-bitcoins-uk/").search('#bb-tablepress-sell tr')[10].search('td')[4].text.gsub('£','').gsub(',','').to_f
+  def self.usd_per_btc
+    Mechanize.new.get("https://www.coinmath.com/bitcoin/usd").search('.price.number-with-commas')[0].text.to_f
   end  
   
-  def gbp_per_unit
-    btc_per_unit * Holding.gbp_per_btc
+  def usd_per_unit
+    btc_per_unit * Holding.usd_per_btc
   end
   
   def valuation
-    (amount*gbp_per_unit).round
+    (amount*usd_per_unit).round
   end
   
   validates_presence_of :description, :currency, :amount
