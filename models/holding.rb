5c262ba4618767589c2ca3d4bc685503c75e0c30
postscript07@gmail.com <postscript07@gmail.com>
2017-12-08 19:09:40 -0600

    .

--- models/holding.rb
+++ models/holding.rb
@@ -2,21 +2,17 @@ class Holding
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :description, :type => String
+  field :wallet, :type => String
   field :currency, :type => String
   field :amount, :type => Float
     
   def btc_per_unit    
-    return 1 if currency == 'bitcoin'
-    begin
-      Mechanize.new.get("https://www.coinmath.com/#{currency}/btc").search('.price.number-with-commas')[0].text.to_f
-    rescue
-      0
-    end
+    return 1 if currency == 'btc'
+    Mechanize.new.get("https://www.coingecko.com/en/coins/#{currency}").search('[data-price-btc]').attr('data-price-btc').value.to_f
   end
   
   def self.usd_per_btc
-    Mechanize.new.get("https://www.coinmath.com/bitcoin/usd").search('.price.number-with-commas')[0].text.to_f
+    Mechanize.new.get('https://www.coingecko.com/en/coins/bitcoin').search('[data-price-btc]').text.strip.gsub('$','').gsub(',','').to_f
   end  
   
   def self.gbp_per_usd
@@ -31,11 +27,11 @@ class Holding
     (amount*usd_per_unit).round
   end
   
-  validates_presence_of :description, :currency, :amount
+  validates_presence_of :wallet, :currency, :amount
         
   def self.admin_fields
     {
-      :description => :text,
+      :wallet => :text,
       :currency => :text,
       :amount => :number
     }
