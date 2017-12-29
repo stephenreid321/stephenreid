76c46e71d547730c30d43d36ba975560d735a9db
postscript07@gmail.com <postscript07@gmail.com>
2017-12-28 22:12:00 -0500

    .

---
+++ models/highlight.rb
@@ -0,0 +1,34 @@
+class Highlight
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  extend Dragonfly::Model    
+
+  field :name, :type => String
+  field :website, :type => String  
+  field :image_uid, :type => String
+  
+  # Dragonfly
+  dragonfly_accessor :image
+  before_validation do
+    if self.image
+      begin
+        self.image.format
+      rescue        
+        errors.add(:image, 'must be an image')
+      end
+    end
+  end     
+  
+  validates_presence_of :name
+  validates_uniqueness_of :name
+        
+  def self.admin_fields
+    {
+      :name => :text,
+      :website => :url,
+      :image_url => :text,
+      :image => :image
+    }
+  end
+    
+end
