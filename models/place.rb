6727594ba9c6ecb08c08856546edf644c9e91fd7
postscript07@gmail.com <postscript07@gmail.com>
2017-12-29 20:15:10 -0500

    .

--- models/place.rb
+++ models/place.rb
@@ -33,7 +33,6 @@ class Place
       "Landscapes that have inspired me" => 'landscapes',
       "Favourite shops and restaurants" => 'shops',
       "Other great places and processes" => 'other',
-      "Technologies I'm skilled at" => 'tech',
       "Plans for the future" => 'upcoming',
     }
   end
