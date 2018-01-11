30596df7dff1169db5c6efcee9621d9f83ad270e
postscript07@gmail.com <postscript07@gmail.com>
2018-01-10 21:41:11 -0600

    .

--- models/holding.rb
+++ models/holding.rb
@@ -7,16 +7,16 @@ class Holding
   field :symbol, :type => String
   field :units, :type => Float
   
-  def cryptocompare
-    @cryptocompare ||= JSON.parse(Mechanize.new.get("https://min-api.cryptocompare.com/data/price?fsym=#{symbol}&tsyms=USD,GBP").body)
-  end
-  
-  def usd_per_unit    
-    @cryptocompare['USD']
+  def usd_per_unit
+    @usd_per_unit ||= Mechanize.new.get("https://coinmarketcap.com/currencies/#{currency}").search('#quote_price .text-large2')[0].text.to_f
   end  
-              
+          
+  def self.gbp_per_usd
+    @gbp_per_usd ||= JSON.parse(Mechanize.new.get('https://api.fixer.io/latest?base=USD&symbols=GBP').body)['rates']['GBP']
+  end
+    
   def gbp_per_unit
-    @cryptocompare['GBP']
+    usd_per_unit * Holding.gbp_per_usd
   end  
   
   def usd_value
