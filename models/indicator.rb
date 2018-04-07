e2776933ee49b5f3f16f4e4d6b0eb7ae3719e55c
Stephen Reid <postscript07@gmail.com>
2018-04-07 09:02:23 +0200

    .

--- models/indicator.rb
+++ models/indicator.rb
@@ -19,7 +19,7 @@ class Indicator
   def correct?
     timestamps = Indicator.order('timestamp asc').pluck(:timestamp).uniq
     i = timestamps.index(timestamp)
-    price = value.to_f
+    price = Indicator.find_by(timestamp: timestamps[i], name: 'price').value.to_f
     if price_indicator_next = Indicator.find_by(timestamp: timestamps[i+1], name: 'price')
       price_next = price_indicator_next.value.to_f
       case value
