b6e010f1ad1ed923fe2b36177ff9e75c260c5881
Stephen Reid <stephen@stephenreid.net>
2018-10-28 11:22:25 +0000

    .

--- models/tarot_card.rb
+++ models/tarot_card.rb
@@ -1,13 +1,26 @@
 class TarotCard
   include Mongoid::Document
   include Mongoid::Timestamps
-
+  extend Dragonfly::Model   
+  
   field :name, :type => String
   field :description, :type => String
   field :teachmetarot_url, :type => String
   
   belongs_to :tarot_number
   belongs_to :tarot_suit, optional: true  
+  
+  # Dragonfly
+  dragonfly_accessor :image
+  before_validation do
+    if self.image
+      begin
+        self.image.format
+      rescue        
+        errors.add(:image, 'must be an image')
+      end
+    end
+  end     
   
   def self.majors
     ['fool', 'magician', 'high priestess', 'empress', 'emperor', 'hierophant', 'lovers', 'chariot', 'strength', 'hermit', 'wheel of fortune',
