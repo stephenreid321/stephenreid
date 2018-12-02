bfa5367571950a63983d39e895fd261f56afef80
Stephen Reid <stephen@stephenreid.net>
2018-12-02 22:17:44 +0000

    .

--- app/app.rb
+++ app/app.rb
@@ -47,9 +47,66 @@ module ActivateApp
       erb :not_found, :layout => :application
     end
 
-    get :home, :map => '/' do
+    get '/', :cache => true do
       erb :home
     end
+
+    get '/podcast', :cache => true do
+      @title = 'Podcast'
+      erb :podcast
+    end
+
+    get '/calendar', :cache => true do
+      @title = 'Calendar'
+      erb :calendar
+    end
+     
+    get '/habits', :cache => true do
+      @title = 'Habits'
+      erb :habits
+    end
+
+    get '/music', :cache => true do
+      erb :music
+    end
+
+    get '/tarot', :cache => true do
+      @title = 'Tarot'
+      @og_image = "http://#{ENV['DOMAIN']}/images/tarotcards.jpg"
+      erb :tarot
+    end
+    
+    get '/books', :cache => true do
+      @title = 'Books'
+      erb :books
+    end
+
+    get '/darknet', :cache => true do
+      @title = 'Step-by-step guide to the darknet'
+      @og_desc = 'A step-by-step guide to using a darknet marketplace. Start to finish, it takes a couple of hours.'
+      @og_image = "http://#{ENV['DOMAIN']}/images/marketplace/tor.png"
+      erb :darknet
+    end
+
+    get '/why-use-the-darknet', :cache => true do
+      @title = 'Why use the darknet to obtain psychedelics?'
+      @og_desc = 'Why using the darknet to obtain psychedelics is better for you and better for society'
+      erb :why_use_the_darknet
+    end
+    
+    
+    
+    get '/places-plans' do
+      @title = 'Places & Plans'
+      if params[:map]
+        @organisations = Organisation.all
+        @organisations = @organisations.where(category: 'upcoming') if params[:plans]
+        @view = 'map'
+        erb :map
+      else
+        erb :organisations
+      end
+    end      
     
     get '/link' do
       redirect Fragment.find_by(slug: 'link').body
@@ -62,86 +119,14 @@ module ActivateApp
     get '/podcasts' do
       redirect '/podcast'
     end
-
-    get '/podcast' do
-      @title = 'Podcast'
-      erb :podcast
-    end
-
-    get '/calendar' do
-      @title = 'Calendar'
-      erb :calendar
-    end
-
-    get '/places-plans' do
-      @title = 'Places & Plans'
-      if params[:map]
-        @organisations = Organisation.all
-        @organisations = @organisations.where(category: 'upcoming') if params[:plans]
-        @view = 'map'
-        erb :map
-      else
-        erb :organisations
-      end
-    end
-     
-    get '/habits' do
-      @title = 'Habits'
-      erb :habits
-    end
-
-    get '/music' do
-      erb :music
-    end
-
-    get '/tarot' do
-      @title = 'Tarot'
-      @og_image = "http://#{ENV['DOMAIN']}/images/tarotcards.jpg"
-      erb :tarot
-    end
-    
-    get '/books' do
-      @title = 'Books'
-      erb :books
-    end
-
+        
     get '/gh' do
       redirect 'https://www.google.co.uk/maps/place/Greenhouse/@51.5529027,-0.0879017,15z/data=!4m2!3m1!1s0x0:0x9850520d11f22809'
     end
     
     get '/ps' do
       redirect 'https://www.google.com/maps/place/The+Psychedelic+Society/@51.547382,-0.0449452,17z/data=!4m12!1m6!3m5!1s0x48761dff684f311b:0xa492a62a16335e19!2sThe+Psychedelic+Society!8m2!3d51.547382!4d-0.0427565!3m4!1s0x48761dff684f311b:0xa492a62a16335e19!8m2!3d51.547382!4d-0.0427565'
-    end
-
-    get '/art' do
-      @title = 'Art'
-      erb :art
-    end
-
-    get '/art-block' do
-      partial :art_block, :locals => {:title => params[:title], :link => params[:link], :image_url => params[:image_url]}
-    end
-
-    get '/arena' do
-      erb :arena
-    end
-
-    get '/block' do
-      partial :block, :locals => {:title => params[:title], :link => params[:link], :image_url => params[:image_url]}
-    end
-
-    get '/darknet' do
-      @title = 'Step-by-step guide to the darknet'
-      @og_desc = 'A step-by-step guide to using a darknet marketplace. Start to finish, it takes a couple of hours.'
-      @og_image = "http://#{ENV['DOMAIN']}/images/marketplace/tor.png"
-      erb :darknet
-    end
-
-    get '/why-use-the-darknet' do
-      @title = 'Why use the darknet to obtain psychedelics?'
-      @og_desc = 'Why using the darknet to obtain psychedelics is better for you and better for society'
-      erb :why_use_the_darknet
-    end
+    end    
 
     get '/:slug' do
       if @fragment = Fragment.find_by(slug: params[:slug], page: true)
