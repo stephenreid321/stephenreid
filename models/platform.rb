1e76bc2944c51b0830b62a788361f79ac343be1b
Stephen Reid <stephen@stephenreid.net>
2018-12-23 16:38:13 +0000

    .

---
+++ models/platform.rb
@@ -0,0 +1,4 @@
+class Platform < Airrecord::Table
+  self.base_key = ENV['AIRTABLE_BASE_KEY']
+  self.table_name = "Platforms"        
+end
--- models/social_network.rb
+++
@@ -1,4 +0,0 @@
-class SocialNetwork < Airrecord::Table
-  self.base_key = ENV['AIRTABLE_BASE_KEY']
-  self.table_name = "Social networks"        
-end
