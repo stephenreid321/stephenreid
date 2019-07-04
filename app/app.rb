78c75980a56a22339c1b5a59f282d752f33d7104
Stephen Reid <stephen@stephenreid.net>
2019-07-04 09:43:30 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -94,7 +94,8 @@ module ActivateApp
                    
     get '/places-plans' do
       @title = 'Places & Plans'
-      @organisations = params[:plans] ? Organisation.all(filter: "AND({Interest} = 'future', {Latitude} != '', {Longitude} != '')") : Organisation.all(filter: "AND({Latitude} != '', {Longitude} != '')")
+      @organisations = Organisation.all(filter: "AND({Latitude} != '', {Longitude} != '')")
+      @organisations = @organisations.select { |organisation| params[:categories].include?(organisation['Interest name']) } if params[:categories]
       erb :organisations
     end      
         
