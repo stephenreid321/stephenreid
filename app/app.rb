dd82e9982ee3210b32b217edca4c8fc22121ac7d
Stephen Reid <stephen@stephenreid.net>
2019-07-19 15:40:00 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -98,8 +98,7 @@ module ActivateApp
                    
     get '/places-plans' do
       @title = 'Places & Plans'
-      @organisations = params[:category] ? Organisation.all(filter: "AND({Interest name} = '#{params[:category]}', {Latitude} != '', {Longitude} != '')") : Organisation.all(filter: "AND({Latitude} != '', {Longitude} != '')")
-      erb :organisations
+      erb :places
     end      
         
     get '/link' do
