acbace92a0bb3602527ed885ee03d1664d5e78fd
Stephen Reid <stephen@stephenreid.net>
2019-09-21 20:59:47 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -40,7 +40,7 @@ module ActivateApp
       end      
       fix_params!
       @og_desc = 'Social entrepreneur, activist and public speaker'
-      @og_image = "http://#{ENV['DOMAIN']}/images/link2.png"
+      @og_image = "http://#{ENV['DOMAIN']}/images/link3.png"
     end
 
     error do
