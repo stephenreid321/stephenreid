ff8c1da79e0e6f230125f8fd27f38507fe01684d
Stephen Reid <stephen@stephenreid.net>
2019-10-25 17:29:49 +0100

    .

--- models/blog_post.rb
+++ models/blog_post.rb
@@ -1,4 +1,9 @@
 class BlogPost < Airrecord::Table
   self.base_key = ENV['AIRTABLE_BASE_KEY']
   self.table_name = "Blog posts"        
+  
+  def post
+    Post.all(filter: "{Link} = 'https://stephenreid.net/blog/#{self['Slug']}'").first
+  end
+  
 end
