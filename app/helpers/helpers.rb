e75c35701ceeaced466d2fb2352d8c731ffb5d6e
Stephen Reid <stephen@stephenreid.net>
2019-11-07 13:10:25 +0000

    .

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,5 +3,10 @@ ActivateApp::App.helpers do
   def timeago(x)
     %Q{<abbr class="timeago" title="#{x.iso8601}">#{x}</abbr>}
   end  
+  
+  def md(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    markdown.render(text)      
+  end
          
 end
