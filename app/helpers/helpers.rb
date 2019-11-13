fca41d5bfb580192c5b4dae7fafb4700dedb952e
Stephen Reid <stephen@stephenreid.net>
2019-11-13 20:06:17 +0000

    .

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -8,5 +8,9 @@ ActivateApp::App.helpers do
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
     markdown.render(text)      
   end
+  
+  def cp(slug)
+    cache(slug, :expires => 1.hour.to_i) do; partial :"#{slug}"; end
+  end
          
 end
