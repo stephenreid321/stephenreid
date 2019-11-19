ca96fea70b2c4a11e843fb12dbc3ddd2050fcf8e
Stephen Reid <stephen@stephenreid.net>
2019-11-19 17:20:31 +0000

    .

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,8 +6,11 @@ ActivateApp::App.helpers do
   
   def md(text)
     if text.starts_with?('http')
+      if URI(text).host == 'www.dropbox.com'
+        text = text.gsub('dl=0','dl=1')
+      end
       text = open(text).read.force_encoding('utf-8')
-    end
+    end    
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
     markdown.render(text)      
   end
