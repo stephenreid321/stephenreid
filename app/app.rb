71b7f5f17467483e697ce74a6a9cdebc43ac92c9
Stephen Reid <stephen@stephenreid.net>
2020-09-02 18:07:15 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -134,35 +134,6 @@ module StephenReid
       redirect '/groups?r=1'
     end
 
-    get '/iconomi' do
-      @p = false
-      @favicon = 'moon.png'
-      erb :iconomi
-    end
-
-    get '/iconomi/:p' do
-      @p = (params[:p] == ENV['ICN_PASSWORD'])
-      @favicon = 'moon.png'
-      erb :iconomi
-    end
-
-    post '/iconomi/:p/bail' do
-      halt unless params[:p] == ENV['ICN_PASSWORD']
-      redirect "/iconomi/#{ENV['ICN_PASSWORD']}/bail"
-    end
-
-    get '/iconomi/:p/bail' do
-      halt unless params[:p] == ENV['ICN_PASSWORD']
-      # Strategy.bail
-      redirect "/iconomi/#{ENV['ICN_PASSWORD']}"
-    end
-
-    get '/iconomi/:p/post_structure' do
-      halt unless params[:p] == ENV['ICN_PASSWORD']
-      Strategy.post_structure
-      redirect "/iconomi/#{ENV['ICN_PASSWORD']}"
-    end
-
     get '/substack' do
       @from = params[:from] ? Date.parse(params[:from]) : Date.today
       erb :substack
