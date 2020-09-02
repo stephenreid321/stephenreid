2dde009ec891539f03da1bb714f4c7bdde41834c
Stephen Reid <stephen@stephenreid.net>
2020-09-02 18:06:33 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -140,15 +140,27 @@ module StephenReid
       erb :iconomi
     end
 
-    post '/iconomi/bail' do
-      Strategy.bail
-      redirect "/iconomi/#{ENV['ICN_PASSWORD']}"
-    end
-
     get '/iconomi/:p' do
       @p = (params[:p] == ENV['ICN_PASSWORD'])
       @favicon = 'moon.png'
       erb :iconomi
+    end
+
+    post '/iconomi/:p/bail' do
+      halt unless params[:p] == ENV['ICN_PASSWORD']
+      redirect "/iconomi/#{ENV['ICN_PASSWORD']}/bail"
+    end
+
+    get '/iconomi/:p/bail' do
+      halt unless params[:p] == ENV['ICN_PASSWORD']
+      # Strategy.bail
+      redirect "/iconomi/#{ENV['ICN_PASSWORD']}"
+    end
+
+    get '/iconomi/:p/post_structure' do
+      halt unless params[:p] == ENV['ICN_PASSWORD']
+      Strategy.post_structure
+      redirect "/iconomi/#{ENV['ICN_PASSWORD']}"
     end
 
     get '/substack' do
