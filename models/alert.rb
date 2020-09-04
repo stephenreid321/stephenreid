93dbd0b15a6c1d7ae83e74bea9e8157d20fc4ef3
Stephen Reid <stephen@stephenreid.net>
2020-09-04 14:42:21 +0100

    .

--- models/alert.rb
+++ models/alert.rb
@@ -12,7 +12,7 @@ class Alert
   validates_uniqueness_of :ccowl_id
 
   after_create do
-    Strategy.bail(text: text)
+    Strategy.bail(text: text) if value.negative?
   end
 
   def self.admin_fields
