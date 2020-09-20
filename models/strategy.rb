53fa640ee40082892fb14cff4ff8060fb81342b9
Stephen Reid <stephen@stephenreid.net>
2020-09-20 09:46:07 +0100

    .

--- models/strategy.rb
+++ models/strategy.rb
@@ -172,6 +172,10 @@ class Strategy
       assets += [['ETH', (1 - t).round(4)]]
     end
 
+    assets = Hash[assets.map { |k, v| [k, v.floor(4)] }]
+    t = assets.map { |_k, v| v }.sum
+    assets['ETH'] += (1 - t).round(4)
+
     t = assets.map { |_k, v| v }.sum
     raise Strategy::RoundingError(assets.to_json) unless t == 1
 
