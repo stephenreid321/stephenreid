78eabbc9b4ca212d8e5fe5ae4767fba8d9c75ff0
Stephen Reid <stephen@stephenreid.net>
2020-11-22 18:29:25 +0000

    .

--- app/jekyll_blog/_plugins/custom_markdown_processor.rb
+++ app/jekyll_blog/_plugins/custom_markdown_processor.rb
@@ -5,9 +5,8 @@ class Jekyll::Converters::Markdown::CustomMarkdownProcessor
   end
 
   def convert(text)
-    text = text.gsub(/\A---(.|\n)*?---/, '')
-    text = text.gsub(/==(.|\n)*?==/) { |m| "<mark>#{m[2..-3]}</mark>" }
-    markdown = ::Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
-    markdown.render(text)
+    text.gsub!(/\A---(.|\n)*?---/, '')
+    text.gsub!(/==(.|\n)*?==/) { |m| "<mark>#{m[2..-3]}</mark>" }
+    ::Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true).render(text)
   end
 end
