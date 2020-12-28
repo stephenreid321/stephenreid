89f37fe0831c9efad72018c4baa0e2f07d1a7efd
Stephen Reid <stephen@stephenreid.net>
2020-12-28 20:10:46 +0000

    .

--- app/controllers/crypto.rb
+++ app/controllers/crypto.rb
@@ -80,7 +80,7 @@ StephenReid::App.controller do
     else
       cache_key = "coin_#{params[:slug]}"
       expire(cache_key) if coin.units.nil? || (coin.units && coin.units.zero?)
-      cache(cache_key, expires: 5*60) do
+      cache(cache_key, expires: 5 * 60) do
         coin.remote_update
         partial :'crypto/coin', locals: { coin: coin }
       end
@@ -91,6 +91,7 @@ StephenReid::App.controller do
     sign_in_required!
     coin = Coin.find_by(slug: params[:slug])
     coin.update_attribute(:tag_id, nil)
+    expire("coin_#{params[:slug]}")
     200
   end
 
@@ -99,6 +100,7 @@ StephenReid::App.controller do
     coin = Coin.find_by(slug: params[:slug])
     coin.update_attribute(:starred, true)
     coin.remote_update
+    expire("coin_#{params[:slug]}")
     200
   end
 
@@ -107,6 +109,7 @@ StephenReid::App.controller do
     coin = Coin.find_by(slug: params[:slug])
     coin.update_attribute(:starred, nil)
     coin.remote_update
+    expire("coin_#{params[:slug]}")
     200
   end
 
