f6b7e0f809a20f0e1fde1fb6f2dded5e2c27f9d7
Stephen Reid <stephen@stephenreid.net>
2021-02-14 17:51:00 +0000

    .

--- models/airrecord/tarot_card.rb
+++ models/airrecord/tarot_card.rb
@@ -4,4 +4,16 @@ class TarotCard < Airrecord::Table
 
   belongs_to :tarot_suit, class: 'TarotSuit', column: 'Tarot suit'
   belongs_to :tarot_number, class: 'TarotNumber', column: 'Tarot number'
+
+  def update_image
+    agent = Mechanize.new
+    page = agent.get('https://blog.goo.ne.jp/valet_de_coupe/e/cb9361a10db2c819ee498cf250f66813')
+    page.search('.entry-body')
+    srcs = page.search('.entry-body img').map { |img| img.parent.attr('href') }
+    srcs.delete_at(1)
+    suits = %w[wands cups swords pentacles]
+    offset = tarot_suit ? (21 + suits.index(tarot_suit['Name']) * 14) : 0
+    self['Image'] = [{ url: srcs[offset + tarot_number['Value']] }]
+    save
+  end
 end
