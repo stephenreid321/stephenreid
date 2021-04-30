65ed627f341b6bd5f56480b2e3d95fc0bc057637
Stephen Reid <stephen@stephenreid.net>
2021-04-30 17:52:44 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -37,7 +37,7 @@ module StephenReid
       fix_params!
       Time.zone = 'London'
       @og_desc = 'Co-operative technologist and cultural changemaker'
-      @og_image = "https://api.apiflash.com/v1/urltoimage?access_key=#{ENV['APIFLASH_KEY']}&url=https://stephenreid.net#{request.path}&width=1280&height=591"
+      @og_image = "https://api.apiflash.com/v1/urltoimage?access_key=#{ENV['APIFLASH_KEY']}&url=https://stephenreid.net#{request.path}&width=1280&height=672"
     end
 
     error do
