db0f423ef92974c5e9a5903a6316c390b9184567
Stephen Reid <stephen@stephenreid.net>
2022-01-02 13:49:08 +0000

    .

--- models/airrecord/discord_server.rb
+++ models/airrecord/discord_server.rb
@@ -2,3 +2,16 @@ class DiscordServer < Airrecord::Table
   self.base_key = ENV['AIRTABLE_BASE_KEY']
   self.table_name = 'Discord servers'
 end
+
+# jQuery('[data-list-item-id^=guildsnav]').each(function() {
+#
+#   name = jQuery(this).attr('aria-label')
+#   parts = name.split(', ')
+#   name = parts[parts.length - 1]
+#   href = jQuery(this).attr('href') || ''
+#   parts = href.split('/')
+#   id = parts[parts.length-2]
+#   src = jQuery(this).find('img').attr('src')
+#   console.log(name.trim() + ',' + id + ',' + src)
+#
+# })
--- models/airrecord/podcast.rb
+++ models/airrecord/podcast.rb
@@ -2,3 +2,7 @@ class Podcast < Airrecord::Table
   self.base_key = ENV['AIRTABLE_BASE_KEY']
   self.table_name = 'Podcasts'
 end
+
+# jQuery('.LunqxlFIupJw_Dkx6mNx').each(function() {
+#   console.log('"'+jQuery('a',this).attr('title')+'","'+jQuery('span',this).text()+'","'+jQuery('a',this).attr('href')+'","'+jQuery('img',this).attr('src')+'"')
+# })
