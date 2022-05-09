e514ee41cbeed94ef83f25e3578eea5b07507f45
Stephen Reid <stephen@stephenreid.net>
2022-05-09 16:18:26 +0100

    .

--- models/strategy.rb
+++ models/strategy.rb
@@ -201,8 +201,8 @@ class Strategy
     Strategy.active_mature.and(:ticker.ne => 'DECENTCOOP').each_with_index do |strategy, i|
       puts "#{i + 1}/#{count}"
       strategy.holdings.each do |holding|
-        asset = if %w[USDC USDT TUSD DAI].include?(holding.asset.ticker)
-                  Asset.find_by(ticker: 'UST')
+        asset = if %w[UST USDT TUSD DAI].include?(holding.asset.ticker)
+                  Asset.find_by(ticker: 'USDC')
                 else
                   holding.asset
                 end
