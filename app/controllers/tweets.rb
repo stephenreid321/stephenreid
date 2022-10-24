d4ca0883b9084ca1e6faae8ae64af484e006b53c
Stephen Reid <stephen@stephenreid.net>
2022-10-24 15:27:18 +0200

    .

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -4,13 +4,13 @@ StephenReid::App.controller do
   end
 
   get '/tweets' do
-    redirect '/tweets/1h/likes'
+    redirect '/tweets/6h/likes'
   end
 
   get '/tweets/:timeframe/:likes_or_rts' do
     @tweets = Tweet.all.select do |t|
       t = t.data
-      t['age'] < (case params[:timeframe]; when '6h' then 6.hours; when '24h' then 24.hours; when '7d' then 7.days; end)
+      t['age'] < (case params[:timeframe]; when '6h' then 6.hours; when '24h' then 24.hours; when '7d' then 7.days; end) && t['age'] >= (case params[:timeframe]; when '6h' then 0; when '24h' then 6.hours; when '7d' then 24.hours; end)
     end.sort_by do |t|
       t = t.data
       t["#{params[:likes_or_rts]}_per_follower_per_second"]
