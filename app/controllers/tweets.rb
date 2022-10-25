425446390c80c4e52a16755ce81a84c3c27445dc
Stephen Reid <stephen@stephenreid.net>
2022-10-25 18:25:39 +0200

    .

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -12,6 +12,7 @@ StephenReid::App.controller do
                   when '6h' then 6.hours
                   when '12h' then 12.hours
                   when '24h' then 24.hours
+                  when '3d' then 3.days
                   when '7d' then 7.days
                   end) && t['age'] >= (case params[:timeframe]
                                        when '1h' then 0
@@ -19,7 +20,8 @@ StephenReid::App.controller do
                                        when '6h' then 3.hours
                                        when '12h' then 6.hours
                                        when '24h' then 12.hours
-                                       when '7d' then 24.hours
+                                       when '3d' then 24.hours
+                                       when '7d' then 3.days
                                        end)
     end.sort_by do |t|
       t = t.data
