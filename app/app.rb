0f90aebd9a89675657786f0720074a4cac7f886d
Stephen Reid <stephen@stephenreid.net>
2023-02-14 15:24:46 +0100

    .

--- app/app.rb
+++ app/app.rb
@@ -62,7 +62,7 @@ module StephenReid
 
     get '/sign_in/:code' do
       if params[:code] == ENV['SIGN_IN_CODE']
-        session[:account_id] = account.id.to_s
+        session[:account_id] = Account.find_by(admin: true)
         flash.now[:success] = 'Signed in!'        
       end
       redirect '/'
