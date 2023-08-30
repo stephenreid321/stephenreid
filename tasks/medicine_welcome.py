4c6115f775b533dc4e86664a42ca4b36e2c7673b
Stephen Reid <stephen@stephenreid.net>
2023-08-30 09:09:06 +0100

    .

--- tasks/medicine_welcome.py
+++ tasks/medicine_welcome.py
@@ -3,7 +3,7 @@ from telegram import Bot
 import asyncio
 
 
-async def send_message():
+async def medicine_welcome():
     token = os.environ.get("TELEGRAM_BOT_TOKEN")
     bot = Bot(token=token)
     message = "Welcome to all new members! Please make sure you've read the pinned post 👆, and do try to keep discussion in this channel to a minimum - instead, post in your local Circle(s) 🙏"
@@ -12,4 +12,4 @@ async def send_message():
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(send_message())
+    loop.run_until_complete(medicine_welcome())
