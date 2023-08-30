f72d7ca654fc85c8a846fdda85f6bb0a8577cc7a
Stephen Reid <stephen@stephenreid.net>
2023-08-30 09:07:37 +0100

    .

--- worker.py
+++ worker.py
@@ -6,7 +6,8 @@ import asyncio
 async def send_message():
     token = os.environ.get("TELEGRAM_BOT_TOKEN")
     bot = Bot(token=token)
-    await bot.send_message(chat_id="@stephenreid321", text="hey there")
+    message = "Welcome to all new members! Please make sure you've read the pinned post 👆, and do try to keep discussion in this channel to a minimum - instead, post in your local Circle(s) 🙏"
+    await bot.send_message(chat_id=-1001592559430, reply_to_message_id=2, text=message)
 
 
 if __name__ == "__main__":
