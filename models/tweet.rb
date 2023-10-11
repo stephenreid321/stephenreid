6b72cb3d7933b9901b0f142134ebb182536df552
Stephen Reid <stephen@stephenreid.net>
2023-10-11 16:19:51 +0200

    .

--- models/tweet.rb
+++ models/tweet.rb
@@ -68,7 +68,7 @@ class Tweet
 
   def self.nitter_user(username, timeline, cursor: nil)
     a = Mechanize.new
-    a.set_proxy(ENV['PROXY'].split(':')[0], ENV['PROXY'].split(':')[1], ENV['PROXY_USERNAME'], ENV['PROXY_PASSWORD'])
+    a.set_proxy(ENV['PROXY'].split(':')[0], ENV['PROXY'].split(':')[1], ENV['PROXY_USERNAME'], ENV['PROXY_PASSWORD']) if ENV['PROXY_ACTIVATED']
     oldest_tweet_in_cursor_created_at = nil
     url = "https://#{ENV['NITTER_DOMAIN']}/#{username}?cursor=#{cursor}"
     puts url
