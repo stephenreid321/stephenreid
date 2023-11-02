f38793e23f4cbbcdb7d916f2e1d09f86bdb2df6f
Stephen Reid <stephen@stephenreid.net>
2023-11-02 21:00:57 +0000

    .

--- app/assets/javascripts/toc.js
+++ app/assets/javascripts/toc.js
@@ -8,22 +8,21 @@
 
     return this.each(function () {
       const tocElement = $(this);
-      const selectors = settings.selectors.split(',');
       let html = '<ul>';
       let headingIds = {};
 
-      selectors.forEach(selector => {
-        const headingTag = selector.trim();
+      const headings = $(settings.container).find(settings.selectors).toArray();
+      headings.sort((a, b) => a.offsetTop - b.offsetTop);
+
+      headings.forEach((heading, index) => {
+        const headingTag = heading.tagName.toLowerCase();
+        const id = heading.id || `${headingTag}-${index}`;
+        heading.id = id;
         if (!headingIds[headingTag]) {
           headingIds[headingTag] = [];
         }
-
-        $(settings.container).find(headingTag).each(function (index, heading) {
-          const id = heading.id || `${headingTag}-${index}`;
-          heading.id = id;
-          headingIds[headingTag].push(id);
-          html += `<li class="toc-${headingTag}"><a href="#${id}">${heading.textContent}</a></li>`;
-        });
+        headingIds[headingTag].push(id);
+        html += `<li class="toc-${headingTag}"><a href="#${id}">${heading.textContent}</a></li>`;
       });
 
       html += '</ul>';
@@ -44,20 +43,19 @@
       const highlightTOCOnScroll = () => {
         let scrollPosition = $(window).scrollTop();
 
-        Object.keys(headingIds).forEach(headingTag => {
-          headingIds[headingTag].forEach(id => {
-            const headingElement = $('#' + id);
-            const headerMarginTop = parseInt(headingElement.css('margin-top'), 10);
-            const offsetTop = headingElement.offset().top - headerMarginTop;
-            const nextIdIndex = headingIds[headingTag].indexOf(id) + 1;
-            const nextHeadingElement = nextIdIndex < headingIds[headingTag].length ? $('#' + headingIds[headingTag][nextIdIndex]) : null;
-            let offsetBottom = nextHeadingElement ? nextHeadingElement.offset().top - parseInt(nextHeadingElement.css('margin-top'), 10) : $(document).height();
+        headings.forEach((heading, index) => {
+          const headingTag = heading.tagName.toLowerCase();
+          const id = heading.id;
+          const headingElement = $('#' + id);
+          const headerMarginTop = parseInt(headingElement.css('margin-top'), 10);
+          const offsetTop = headingElement.offset().top - headerMarginTop;
+          const nextHeadingElement = index < headings.length - 1 ? $('#' + headings[index + 1].id) : null;
+          let offsetBottom = nextHeadingElement ? nextHeadingElement.offset().top - parseInt(nextHeadingElement.css('margin-top'), 10) : $(document).height();
 
-            if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
-              tocElement.find('li').removeClass(settings.activeClass);
-              tocElement.find(`a[href="#${id}"]`).parent().addClass(settings.activeClass);
-            }
-          });
+          if (scrollPosition >= offsetTop && scrollPosition < offsetBottom) {
+            tocElement.find('li').removeClass(settings.activeClass);
+            tocElement.find(`a[href="#${id}"]`).parent().addClass(settings.activeClass);
+          }
         });
       };
 
