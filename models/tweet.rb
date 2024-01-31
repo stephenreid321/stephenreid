d0f29b0b9508f1506d8adda8498cfc01a5430b06
Stephen Reid <stephen@stephenreid.net>
2024-01-31 17:09:28 +0000

    .

--- models/tweet.rb
+++ models/tweet.rb
@@ -70,7 +70,8 @@ class Tweet
     a = Mechanize.new
     a.set_proxy(ENV['PROXY'].split(':')[0], ENV['PROXY'].split(':')[1], ENV['PROXY_USERNAME'], ENV['PROXY_PASSWORD']) if ENV['PROXY_ACTIVATED']
     oldest_tweet_in_cursor_created_at = nil
-    url = "https://#{ENV['NITTER_DOMAIN']}/#{username}?cursor=#{cursor}"
+    nitter_instance = Faraday.get('https://farside.link/nitter').headers['location']
+    url = "#{nitter_instance}/#{username}?cursor=#{cursor}"
     puts url
     page = begin; a.get(url); rescue Mechanize::ResponseCodeError; return; end
     page.search('.timeline .timeline-item .tweet-body').each do |item|
