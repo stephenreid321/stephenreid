e15d693f278c26c2c87790282021af70f99915e8
Stephen Reid <stephen@stephenreid.net>
2024-03-14 08:06:44 +0100

    .

--- app/jekyll_blog/_plugins/custom_markdown_processor.rb
+++ app/jekyll_blog/_plugins/custom_markdown_processor.rb
@@ -1,18 +1,12 @@
-module Jekyll
-  module Converters
-    module Markdown
-      class CustomMarkdownProcessor
-        def initialize(config)
-          require 'redcarpet'
-          @config = config
-        end
+class Jekyll::Converters::Markdown::CustomMarkdownProcessor
+  def initialize(config)
+    require 'redcarpet'
+    @config = config
+  end
 
-        def convert(text)
-          text.gsub!(/\A---(.|\n)*?---/, '')
-          text.gsub!(/==(.|\n)*?==/) { |m| "<mark>#{m[2..-3]}</mark>" }
-          ::Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true).render(text)
-        end
-      end
-    end
+  def convert(text)
+    text.gsub!(/\A---(.|\n)*?---/, '')
+    text.gsub!(/==(.|\n)*?==/) { |m| "<mark>#{m[2..-3]}</mark>" }
+    ::Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true).render(text)
   end
 end
