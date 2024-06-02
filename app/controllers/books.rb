be4f7908c1e21dc892f21d11e2068b73fd8ff14c
Stephen Reid <stephen@stephenreid.net>
2024-06-02 11:14:40 +0200

    .

--- app/controllers/books.rb
+++ app/controllers/books.rb
@@ -6,6 +6,7 @@ StephenReid::App.controller do
 
   get '/books/:slug' do
     @book = Book.all(filter: "{Slug} = '#{params[:slug]}'").first || not_found
+    redirect("https://www.goodreads.com/book/show/#{@book['Book Id']}") if @book['Summary'].blank?
     @title = @book['Title']
     erb :book
   end
