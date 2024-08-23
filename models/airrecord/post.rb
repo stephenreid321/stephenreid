0da33e3406f6ac3e3a19ee221f4aacd22deabbc6
Stephen Reid <stephen@stephenreid.net>
2024-08-23 16:52:44 +0200

    .

--- models/airrecord/post.rb
+++ models/airrecord/post.rb
@@ -158,6 +158,8 @@ class Post < Airrecord::Table
     puts 'downloading'
     if URI(post['Link']).host.include?('youtube.com')
       audio_path = `python tasks/youtube_audio.py "#{post['Link']}"`.strip
+    elsif URI(post['Link']).host.include?('spotify.com')
+      audio_path = `python tasks/youtube_audio.py "#{post['Link']}"`.strip
     elsif URI(post['Link']).host.include?('substack.com')
       agent = Mechanize.new
       r = agent.get(post['Link'])
