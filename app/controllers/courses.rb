b60cd6d8e502508d56cff185cf80e3dba66cbeff
Stephen Reid <stephen@stephenreid.net>
2025-05-19 16:43:53 +0200

    ♻️ Set page title directly in view
    Remove the `@title` instance variable assignment from the
    courses controller.
    
    Add the `<title>` tag directly within the course view template.
    
    This moves the responsibility for setting the HTML page title
    from the controller to the view, adhering better to MVC
    principles.

--- app/controllers/courses.rb
+++ app/controllers/courses.rb
@@ -2,7 +2,6 @@ StephenReid::App.controller do
   get '/courses/:slug' do # cache: true
     expires 1.hour.to_i
     @course = Course.all(filter: "{Slug} = '#{params[:slug]}'").first
-    @title = @course['Name']
     erb :'courses/course', layout: false
   end
 end
