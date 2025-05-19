a27dd39b3f28fada8d77f1a1c5e358d278297066
Stephen Reid <stephen@stephenreid.net>
2025-05-19 16:24:27 +0200

    ✨ Set page title for course view
    Set the `@title` instance variable in the course controller.
    
    This allows the course view template to use the course name
    for the page title, improving SEO and user experience.

--- app/controllers/courses.rb
+++ app/controllers/courses.rb
@@ -2,6 +2,7 @@ StephenReid::App.controller do
   get '/courses/:slug' do # cache: true
     expires 1.hour.to_i
     @course = Course.all(filter: "{Slug} = '#{params[:slug]}'").first
+    @title = @course['Name']
     erb :'courses/course', layout: false
   end
 end
