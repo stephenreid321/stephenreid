0f6578bca1bcf170a28a7eef9912d1496aa40d76
Stephen Reid <stephen@stephenreid.net>
2025-09-11 07:10:19 +0200

    🛂 (throttle.rb): exclude static assets from general IP rate-limiting to reduce false positives 🛂 (throttle.rb): add tighter per-IP throttling for /posts/ and /tao-te-ching/ endpoints to curb abuse

--- lib/throttle.rb
+++ lib/throttle.rb
@@ -5,5 +5,13 @@ Rack::Attack.cache.store = ActiveSupport::Cache::MongoStore.new(nil, collection:
 
 # General rate limiting - allow reasonable browsing but prevent abuse
 Rack::Attack.throttle('requests by ip', limit: 300, period: 5.minutes) do |request|
-  request.ip
+  request.ip unless %w[/fonts/ /images/ /javascripts/ /stylesheets/].any? { |path| request.path.starts_with?(path) }
+end
+
+Rack::Attack.throttle('/posts/', limit: 10, period: 10.seconds) do |request|
+  request.ip if request.path.starts_with?('/posts/')
+end
+
+Rack::Attack.throttle('/tao-te-ching/', limit: 10, period: 10.seconds) do |request|
+  request.ip if request.path.starts_with?('/tao-te-ching/')
 end
