4e91cfb601b41f65e2b40621b5fd4b83ad467e8d
Stephen Reid <stephen@stephenreid.net>
2026-01-05 11:17:39 +0100

    🔧 (config/puma.rb): change default port from 3000 to 3001 to avoid local conflicts

--- config/puma.rb
+++ config/puma.rb
@@ -3,5 +3,5 @@ threads Integer(ENV['MIN_THREADS'] || 3), Integer(ENV['MAX_THREADS'] || 3)
 
 preload_app!
 
-port        ENV['PORT']     || 3000
+port        ENV['PORT']     || 3001
 environment ENV['RACK_ENV'] || 'development'
