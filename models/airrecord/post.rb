df5823b79634c5d2e929941a65adff808ac16b0e
Stephen Reid <stephen@stephenreid.net>
2026-04-14 09:22:26 +0200

    🗑️ Remove unused Twitter and Facebook text generation from tagify method

--- models/airrecord/post.rb
+++ models/airrecord/post.rb
@@ -56,8 +56,6 @@ class Post < Airrecord::Table
   def tagify(skip_linking: false)
     post = self
     json = JSON.parse(post['Iframely'])
-    twitter = post['Title']
-    facebook = post['Title']
     replacements = []
     additions = []
 
@@ -69,52 +67,11 @@ class Post < Airrecord::Table
       additions << term if json['meta'] && [json['meta']['title'], json['meta']['category'], json['meta']['author'], json['meta']['description'], json['meta']['keywords']].any? { |x| x && x.match(i ? /\b#{t}\b/i : /\b#{t}\b/) }
     end
 
-    replacements.each do |term|
-      i = term['Case sensitive'] ? false : true
-      t = term['Name']
-      hashtag = t.include?(' ') ? t.gsub(' ', '_').gsub('-', '_').camelize : t
-      if term['Organisation']
-        twitter = twitter.gsub(i ? /\b#{t}\b/i : /\b#{t}\b/, "@#{term.organisation['Twitter username']}")
-        # facebook = facebook.gsub(i ? /#{t}/i : /#{t}/, "@[#{term.organisation['Facebook page username']}]")
-      else
-        twitter = twitter.gsub(i ? /\b#{t}\b/i : /\b#{t}\b/, "##{hashtag}")
-        facebook = facebook.gsub(i ? /\b#{t}\b/i : /\b#{t}\b/, "##{hashtag}")
-      end
-    end
-
-    twitter_add = []
-    facebook_add = []
-    (additions - replacements).each do |term|
-      t = term['Name']
-      hashtag = t.include?(' ') ? t.gsub(' ', '_').gsub('-', '_').camelize : t
-      if term['Organisation']
-        twitter_add << "@#{term.organisation['Twitter username']}"
-        # facebook_add << "@[#{term.organisation['Facebook page username']}]"
-      else
-        twitter_add << "##{hashtag}"
-        facebook_add << "##{hashtag}"
-      end
-    end
-
     if (organisation = Organisation.all(filter: "{Domain} = '#{URI(post['Link']).host.gsub('www.', '').split('.').last(2).join('.')}'").first)
       post.organisation = organisation
-      unless organisation['No domain tag']
-        twitter_add << "@#{organisation['Twitter username']}"
-        # facebook_add << "@[#{organisation['Facebook page username']}]"
-      end
     end
-
-    (additions + replacements).map { |term| term['Emoji'] }.compact.each do |emoji|
-      twitter_add << emoji
-      facebook_add << emoji
-    end
-
-    twitter_add.uniq.each { |x| twitter = "#{twitter} #{x}" }
-    facebook_add.uniq.each { |x| facebook = "#{facebook} #{x}" }
 
     post.terms = (additions + replacements).uniq
-    post['Twitter text'] = twitter
-    post['Facebook text'] = facebook
     post.save
 
     unless skip_linking
@@ -136,7 +93,7 @@ class Post < Airrecord::Table
       end
     end
 
-    self['Twitter text']
+    self
   end
 
   def self.extract_iframely_metadata(iframely_json)
